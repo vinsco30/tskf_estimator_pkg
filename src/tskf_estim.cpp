b95ce11290b200510efe3338160ef3495e470af3
Vincenzo Scognamiglio <vincenzo.scognamiglio2@unina.it>
2022-12-29 17:03:48 +0100

    Debug estimation fun -- Problema NaN

--- src/tskf_estim.cpp
+++ src/tskf_estim.cpp
@@ -78,6 +78,12 @@ TSKF::TSKF() {
     _P_gamma_kk_kk = Eigen::Matrix<double,4,4>::Zero();
     _P_x_kk_kk = Eigen::Matrix<double,12,12>::Zero();
 
+    _Qgamma.diagonal() << 0.1, 0.1, 0.1, 0.1;
+
+    _Qx.diagonal() << 1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 1e-6, 1e-6, 1e-6, 1e-6, 1e-6, 1e-6;
+
+    _R.diagonal() << 1e-3, 1e-3, 1e-3, 1e-6, 1e-6, 1e-6;
+
     _vec << 0.0, 0.0, 0.0, 0.0, 0.0, -9.81*_Ts, 0.0, 0.0,
     0.0, 0.0, 0.0, 0.0;
 
@@ -217,7 +223,11 @@ bool TSKF::generate_allocation_matrix(Eigen::MatrixXd & allocation_M,
 
 void TSKF::tskf_matrix_generation( Eigen::MatrixXd allocation_M ) {
     Eigen::Matrix<double,12,12> I_12;           //matrice identità
-    _A_k = I_12 + _A_k*_Ts; //matrice sistema linearizzato discretizzata
+    Eigen::Matrix<double,12,12> Ac;
+    I_12.diagonal() << 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+    1.0, 1.0, 1.0, 1.0, 1.0, 1.0;
+    Ac = _A_k; 
+    _A_k = I_12 + Ac*_Ts; //matrice sistema linearizzato discretizzata
 
     Eigen::Matrix<double,12,4> B;
     B = _B_k;    
@@ -243,17 +253,17 @@ void TSKF::estimation() {
 
 
     //Variabili del tskf usate solo in estimation
-    Eigen::Matrix<double,12,4> W_k;             //coupling
-    Eigen::Matrix<double,4,4> P_gamma_kk_k;     //predizione P_gamma
-    Eigen::Vector4d gamma_pred;                 //predizione gamma
-    Eigen::Matrix<double,12,4> V_kk_k;          //coupling
-    Eigen::Matrix<double,6,4> H_kk_k;           //coupling
-    Eigen::Matrix<double,12,12> P_x_kk_k;       //predizione P_x
-    Eigen::Matrix<double,12,1> x_kk_k;          //predizione x
-    Eigen::Matrix<double,12,6> Kx_kk;           //guadagno kf per lo stato
-    Eigen::Matrix<double,6,6> S_kk;             //residuo
-    Eigen::Matrix<double,4,6> K_gamma_kk;       //guadagno kf per gamma
-    Eigen::Matrix<double,4,4> U;                //input di controllo (matrice)
+    Eigen::Matrix<double,12,4> W_k =Eigen::Matrix<double,12,4>::Zero();             //coupling
+    Eigen::Matrix<double,4,4> P_gamma_kk_k = Eigen::Matrix<double,4,4>::Zero();     //predizione P_gamma
+    Eigen::Vector4d gamma_pred = Eigen::Vector4d::Zero();                 //predizione gamma
+    Eigen::Matrix<double,12,4> V_kk_k = Eigen::Matrix<double,12,4>::Zero();          //coupling
+    Eigen::Matrix<double,6,4> H_kk_k = Eigen::Matrix<double,6,4>::Zero();           //coupling
+    Eigen::Matrix<double,12,12> P_x_kk_k = Eigen::Matrix<double,12,12>::Zero();       //predizione P_x
+    Eigen::Matrix<double,12,1> x_kk_k = Eigen::Matrix<double,12,1>::Zero();          //predizione x
+    Eigen::Matrix<double,12,6> Kx_kk = Eigen::Matrix<double,12,6>::Zero();           //guadagno kf per lo stato
+    Eigen::Matrix<double,6,6> S_kk = Eigen::Matrix<double,6,6>::Zero();             //residuo
+    Eigen::Matrix<double,4,6> K_gamma_kk = Eigen::Matrix<double,4,6>::Zero();       //guadagno kf per gamma
+    Eigen::Matrix<double,4,4> U = Eigen::Matrix<double,4,4>::Zero();                //input di controllo (matrice)
     Eigen::Matrix<double,4,4> I_4;              //matrice identità
     Eigen::Matrix<double,12,12> I_12;           //matrice identità
 
@@ -269,35 +279,84 @@ void TSKF::estimation() {
     }
 
     tskf_matrix_generation(allocation_M);
-
-    while (ros::ok() ) {
-
-        U << _u_k[0], 0.0, 0.0, 0.0,
+    // for ( int i=0; i<12; i++) {
+    //     for( int j=0; j<4; j++) {
+    //         cout<<_B_k(i,j)<<" , ";
+    //     }
+    //     cout<<endl;
+    // }
+            U << _u_k[0], 0.0, 0.0, 0.0,
         0.0, _u_k[1], 0.0, 0.0,
         0.0, 0.0, _u_k[2], 0.0,
         0.0, 0.0, 0.0, _u_k[3];
 
         W_k = _A_k*_V_kk_kk - _B_k*U; //coupling equation (26)
+        // // //W_k =  - _B_k*U;
         P_gamma_kk_k = _P_gamma_kk_kk + _Qgamma; //optimal bias estimator (15)
         gamma_pred = _gamma; //OBE (14)
         V_kk_k = W_k*_P_gamma_kk_kk*P_gamma_kk_k.inverse(); //coupling (27)
         H_kk_k = _C_k*V_kk_k; //coupling (28)
-        P_x_kk_k = _A_k*_P_x_kk_kk*_A_k.transpose() + _Qx + W_k*_P_gamma_kk_kk*W_k.transpose()
-        - V_kk_k*P_gamma_kk_k*V_kk_k.transpose(); //BFE (20)
+        P_x_kk_k = _A_k*_P_x_kk_kk*_A_k.transpose() /*+ _Qx + W_k*_P_gamma_kk_kk*W_k.transpose()
+        - V_kk_k*P_gamma_kk_k*V_kk_k.transpose()*/; //BFE (20)
+        P_x_kk_k = P_x_kk_k + _Qx;
         x_kk_k = _A_k*_x_tilde + W_k*_gamma - V_kk_k*_gamma + _vec; //BFE (19)
         _res = _y_kk - _C_k*x_kk_k; //calcolo residui (24)
-        Kx_kk = P_x_kk_k*_C_k.transpose()*(_C_k*P_x_kk_k*_C_k.transpose() + _R).inverse(); //BFE (22)
+        Kx_kk = P_x_kk_k*_C_k.transpose()/*(_C_k*P_x_kk_k*_C_k.transpose() + _R).inverse()*/; //BFE (22)
         S_kk = _C_k*P_x_kk_k*_C_k.transpose() + _R; //calcolo residui (25)
-        K_gamma_kk = P_gamma_kk_k*(H_kk_k.transpose())*(H_kk_k*P_gamma_kk_k*H_kk_k.transpose() + S_kk).inverse(); //OBE (17)
+        K_gamma_kk = P_gamma_kk_k*(H_kk_k.transpose())*(H_kk_k*P_gamma_kk_k*H_kk_k.transpose() + S_kk).inverse();
+
+                for ( int i=0; i<12; i++) {
+            for( int j=0; j<12; j++) {
+                cout<< P_x_kk_k(i,j)<<" , ";
+            }
+            cout<<endl;
+        }
+        cout<<"fine"<<endl;
+    while (ros::ok() ) {
+
+        // U << _u_k[0], 0.0, 0.0, 0.0,
+        // 0.0, _u_k[1], 0.0, 0.0,
+        // 0.0, 0.0, _u_k[2], 0.0,
+        // 0.0, 0.0, 0.0, _u_k[3];
+    // for ( int i=0; i<4; i++) {
+    //     for( int j=0; j<4; j++) {
+    //         cout<<U(i,j)<<" , ";
+    //     }
+    //     cout<<endl;
+    // }
+        // W_k = _A_k*_V_kk_kk - _B_k*U; //coupling equation (26)
+        // // // //W_k =  - _B_k*U;
+        // P_gamma_kk_k = _P_gamma_kk_kk + _Qgamma; //optimal bias estimator (15)
+        // gamma_pred = _gamma; //OBE (14)
+        // V_kk_k = W_k*_P_gamma_kk_kk*P_gamma_kk_k.inverse(); //coupling (27)
+        // H_kk_k = _C_k*V_kk_k; //coupling (28)
+        // P_x_kk_k = _A_k*_P_x_kk_kk*_A_k.transpose() + _Qx + W_k*_P_gamma_kk_kk*W_k.transpose()
+        // - V_kk_k*P_gamma_kk_k*V_kk_k.transpose(); //BFE (20)
+        // x_kk_k = _A_k*_x_tilde + W_k*_gamma - V_kk_k*_gamma + _vec; //BFE (19)
+        // _res = _y_kk - _C_k*x_kk_k; //calcolo residui (24)
+        // Kx_kk = P_x_kk_k*_C_k.transpose()/*(_C_k*P_x_kk_k*_C_k.transpose() + _R).inverse()*/; //BFE (22)
+        // S_kk = _C_k*P_x_kk_k*_C_k.transpose() + _R; //calcolo residui (25)
+        // K_gamma_kk = P_gamma_kk_k*(H_kk_k.transpose())*(H_kk_k*P_gamma_kk_k*H_kk_k.transpose() + S_kk).inverse(); //OBE (17)
 
         //output stimatore
-        _V_kk_kk = V_kk_k - Kx_kk*H_kk_k; //coupling (29)
-        _P_gamma_kk_kk = (I_4 - K_gamma_kk*H_kk_k)*P_gamma_kk_k; //OBE (18)
-        _P_x_kk_kk = (I_12 - Kx_kk*_C_k)*P_x_kk_k; //BFE (23)
-        _x_tilde = x_kk_k + Kx_kk*( _y_kk - _C_k*x_kk_k ); //BFE (21)
-        _gamma = gamma_pred + K_gamma_kk*( _res - H_kk_k*_gamma); //OBE (16)
+        //_V_kk_kk = V_kk_k - Kx_kk*H_kk_k; //coupling (29)
+        //_P_gamma_kk_kk = (I_4 - K_gamma_kk*H_kk_k)*P_gamma_kk_k; //OBE (18)
+        //_P_x_kk_kk = (I_12 - Kx_kk*_C_k)*P_x_kk_k; //BFE (23)
+        // _x_tilde = x_kk_k + Kx_kk*( _y_kk - _C_k*x_kk_k ); //BFE (21)
+        // _gamma = gamma_pred + K_gamma_kk*( _res - H_kk_k*_gamma); //OBE (16)
 
         r.sleep();
+
+        // for ( int i=0; i<6; i++) {
+        //     cout<<_res(i)<<endl;
+        // }    
+        // for ( int i=0; i<12; i++) {
+        //     for( int j=0; j<6; j++) {
+        //         cout<< Kx_kk(i,j)<<" , ";
+        //     }
+        //     cout<<endl;
+        // }
+        // cout<<"fine"<<endl;
 
     }
 
